6be1110a115dfc2f375d7a2f4db24801fe138e78
birnagerald <birnagerald.pro@gmail.com>
2022-02-23 00:09:20 +0100

    update gitignore + add env file

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { DtoGetPokemon } from './dto/get/dtoGetPokemon';
 import { Pokemon } from './model/pokemonType';
 const fetch = require('node-fetch');
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
   async getPokemon({ id }: DtoGetPokemon): Promise<Pokemon> {
     const pokemon: Pokemon = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${id}/`,
+      `${this.configService.get<string>('POKEMON_API_URL')}/pokemon/${id}/`,
     ).then((response: { json: () => Pokemon }) => response.json());
     return pokemon;
   }
